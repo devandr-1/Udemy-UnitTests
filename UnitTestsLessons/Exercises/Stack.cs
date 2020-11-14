8548a773d0d4fa3afa2d7c1679193ca0722c43b6
Andrew ND <andrew.nc.dm@gmail.com>
2020-11-14 15:05:33 +0200

    Exercise 3 is done

---
+++ UnitTestsLessons/Exercises/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestsLessons.Exercises
+{
+    public class Stack<T>
+    {
+        private readonly IList<T> _list = new List<T>();
+
+        public int Count => _list.Count;
+
+        public void Push(T obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException();
+
+            _list.Add(obj);
+        }
+
+        public T Pop()
+        {
+            if (_list.Count == 0)
+                throw new InvalidOperationException();
+
+            int index = _list.Count - 1;
+            
+            var result = _list[index];
+            _list.RemoveAt(index);
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (_list.Count == 0)
+                throw new InvalidOperationException();
+
+            return _list[_list.Count - 1];
+        }
+    }
+}
---
+++ UnitTestsLessonsTests/Exercises/StackTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using UnitTestsLessons.Exercises;
+
+namespace UnitTestsLessonsTests.Exercises
+{
+    [TestFixture]
+    public class StackTests
+    {
+        private Stack<string> _stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stack = new Stack<string>();
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Push_PushNull_ThrowsArgumentNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_PushObject_CountShouldIncrease()
+        {
+            _stack.Push("a");
+
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Pop_WhenCalledOnEmptyStack_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_WhenCalled_ReturnLastElementAndRemoveItFromStack()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+
+            var result = _stack.Pop();
+
+            Assert.That(result, Is.EqualTo("b"));
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Peek_WhenCalledOnEmptyStack_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_WhenCalled_ReturnLastElementWithoutRemoving()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+
+            var result = _stack.Peek();
+
+            Assert.That(result, Is.EqualTo("b"));
+            Assert.That(_stack.Count, Is.EqualTo(2));
+        }
+    }
+}
